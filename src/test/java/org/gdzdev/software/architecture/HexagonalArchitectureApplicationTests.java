33541c775d5bc3ff6173bd50ee8bde24cf96dd0c
Aleks Galdamez <aleksgaldamez2002@gmail.com>
2025-02-16 15:23:19 -0600

    first commit

---
+++ src/test/java/org/gdzdev/software/architecture/HexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gdzdev.software.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
